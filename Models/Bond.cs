f82576be63098855d7979bd7457072a1910e7429
ivp.srath mumbai <srath@ivp.in>
2020-08-31 19:01:40 +0530

    done

--- Models/Bond.cs
+++ Models/Bond.cs
@@ -12,8 +12,8 @@ namespace SecurityMaster_ST.Models
         public string SecurityName { get; set; }
         public string AssetType { get; set; }
         public string InvestmentType { get; set; }
-        public Nullable<double> TradingFactor { get; set; }
-        public Nullable<double> PricingFactor { get; set; }
+        public string TradingFactor { get; set; }
+        public string PricingFactor { get; set; }
         public string ISIN { get; set; }
         public string BBGTicker { get; set; }
         public string BBGUniqueID { get; set; }
@@ -22,8 +22,8 @@ namespace SecurityMaster_ST.Models
         public string FirstCouponDate { get; set; }
         public string Cap { get; set; }
         public string FloorValue { get; set; }
-        public Nullable<int> CouponFrequency { get; set; }
-        public Nullable<double> Coupon { get; set; }
+        public string CouponFrequency { get; set; }
+        public string Coupon { get; set; }
         public string CouponType { get; set; }
         public string Spread { get; set; }
         public string CallableFlag { get; set; }
@@ -32,16 +32,16 @@ namespace SecurityMaster_ST.Models
         public string IssueDate { get; set; }
         public string LastResetDate { get; set; }
         public string Maturity { get; set; }
-        public Nullable<double> CallNotificationMaxDays { get; set; }
-        public Nullable<double> PutNotificationMaxDays { get; set; }
+        public string CallNotificationMaxDays { get; set; }
+        public string PutNotificationMaxDays { get; set; }
         public string PenultimateCouponDate { get; set; }
         public string ResetFrequency { get; set; }
         public string HasPosition { get; set; }
-        public Nullable<double> MacaulayDuration { get; set; }
-        public Nullable<double> Volatility_30D { get; set; }
-        public Nullable<double> Volatility_90D { get; set; }
-        public Nullable<double> Convexity { get; set; }
-        public Nullable<double> AverageVolume_30Day { get; set; }
+        public string MacaulayDuration { get; set; }
+        public string Volatility_30D { get; set; }
+        public string Volatility_90D { get; set; }
+        public string Convexity { get; set; }
+        public string AverageVolume_30Day { get; set; }
         public string PFAssetClass { get; set; }
         public string PFCountry { get; set; }
         public string PFCreditRating { get; set; }
@@ -61,16 +61,16 @@ namespace SecurityMaster_ST.Models
         public string IssuerName { get; set; }
         public string RiskCurrency { get; set; }
         public string PutDate { get; set; }
-        public Nullable<double> PutPrice { get; set; }
-        public Nullable<double> AskPrice { get; set; }
-        public Nullable<double> HighPrice { get; set; }
-        public Nullable<double> LowPrice { get; set; }
-        public Nullable<double> OpenPrice { get; set; }
-        public Nullable<double> Volume { get; set; }
-        public Nullable<double> BidPrice { get; set; }
-        public Nullable<double> LastPrice { get; set; }
+        public string PutPrice { get; set; }
+        public string AskPrice { get; set; }
+        public string HighPrice { get; set; }
+        public string LowPrice { get; set; }
+        public string OpenPrice { get; set; }
+        public string Volume { get; set; }
+        public string BidPrice { get; set; }
+        public string LastPrice { get; set; }
         public string CallDate { get; set; }
-        public Nullable<double> CallPrice { get; set; }
+        public string CallPrice { get; set; }
     }
     
 }
