4ca7ac3f953eab01c71ced92c6a2133bf4cfe88f
ivp.srath mumbai <srath@ivp.in>
2020-08-31 21:53:43 +0530

    aaa

--- Controllers/BondController.cs
+++ Controllers/BondController.cs
@@ -37,7 +37,7 @@ namespace SecurityMaster_ST.Controllers
 
                 return Request.CreateResponse(HttpStatusCode.OK, table);
 
-            }
+            } 
 
         }
 
@@ -190,7 +190,7 @@ namespace SecurityMaster_ST.Controllers
 
                     DataSet ds = reader.AsDataSet();
                     reader.Close();
-
+                    ds.Tables[0].Rows[0].Delete();
 
                     //for each new record call the procedure and insert data
 
